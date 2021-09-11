9a4e2cdfdefdc2da4221fadbadc997aa64e26b56
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-11 14:19:11 -0300

    Criando a controller de autenticação

---
+++ NerdStoreEnterprise/src/services/NSE.Identitidade.API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Identitidade.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identitidade.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        public async Task<IActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+
+        }
+
+        public async Task<IActionResult> Login(Usuariologin usuariologin)
+        {
+
+        }
+
+
+    }
+}
