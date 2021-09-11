32c87e0fcfdbcb7465034d31a8ed7660903518f5
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-11 19:35:17 -0300

    Efetuando o cadastro de um usuário e realizando o login do mesmovia identity

--- NerdStoreEnterprise/src/services/NSE.Identitidade.API/Controllers/AuthController.cs
+++ NerdStoreEnterprise/src/services/NSE.Identitidade.API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Identitidade.API.Models;
 using System;
 using System.Collections.Generic;
@@ -9,9 +10,36 @@ namespace NSE.Identitidade.API.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
         public async Task<IActionResult> Registrar(UsuarioRegistro usuarioRegistro)
         {
+            if (!ModelState.IsValid) return BadRequest();
 
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            // criando um usuário através do Identity
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                // Em caso de usuário criado com sucesso, já faz o login do mesmo no sistema
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            return BadRequest();
         }
 
         public async Task<IActionResult> Login(Usuariologin usuariologin)
