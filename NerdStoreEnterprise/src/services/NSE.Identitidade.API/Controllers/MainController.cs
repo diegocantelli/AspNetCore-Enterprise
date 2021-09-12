8ed80ccda76d3de47798ffeed8b103b22e20ec6b
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-12 14:45:16 -0300

    Criando um método para lidar com os erros da modelState

--- NerdStoreEnterprise/src/services/NSE.Identitidade.API/Controllers/MainController.cs
+++ NerdStoreEnterprise/src/services/NSE.Identitidade.API/Controllers/MainController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,18 @@ namespace NSE.Identitidade.API.Controllers
             {
                 {"Mensagens", Erros.ToArray() }
             }));
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in erros)
+            {
+                AdicionarErrosProcessamento(erro.ErrorMessage)
+            }
+
+            return CustomResponse();
         }
 
         protected bool OperacaoValida()
