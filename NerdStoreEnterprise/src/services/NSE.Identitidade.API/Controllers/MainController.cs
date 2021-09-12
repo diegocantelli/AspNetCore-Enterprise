416c78a889b26a0dd087f9ba3b5d531fd5da4c8b
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-12 14:42:17 -0300

    Criando uma controler customizada para ser herdada pelas demais controllers

---
+++ NerdStoreEnterprise/src/services/NSE.Identitidade.API/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identitidade.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Erros = new List<string>();
+        
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida()) return Ok(result);
+
+            // ValidationProblemDetails -> é um padrão de como a api deve responder aos erros
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]> 
+            {
+                {"Mensagens", Erros.ToArray() }
+            }));
+        }
+
+        protected bool OperacaoValida()
+        {
+            return Erros.Any();
+        }
+
+        protected void AdicionarErrosProcessamento(string erro)
+        {
+            Erros.Add(erro);
+        }
+
+        protected void LimparErrosProcessamento() => Erros.Clear();
+    }
+}
