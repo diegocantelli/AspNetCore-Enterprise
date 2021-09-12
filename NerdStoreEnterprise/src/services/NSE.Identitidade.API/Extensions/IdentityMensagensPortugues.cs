e0466ad60628c5940328b32cbd6ddddd75e092fa
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-12 19:21:34 -0300

    Criando uma classe de extensão para traduzir as mensagens de validação do identity

---
+++ NerdStoreEnterprise/src/services/NSE.Identitidade.API/Extensions/IdentityMensagensPortugues.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identitidade.API.Extensions
+{
+    public class IdentityMensagensPortugues : IdentityErrorDescriber
+    {
+        public override IdentityError DefaultError()
+        {
+            return new IdentityError { Code = nameof(DefaultError), Description = "Ocorreu um erro" };
+        }
+
+        public override IdentityError InvalidUserName(string userName)
+        {
+            return new IdentityError { Code = nameof(InvalidUserName), Description = "Nome de usuário inválido" };
+        }
+
+        public override IdentityError InvalidEmail(string email)
+        {
+            return new IdentityError { Code = nameof(InvalidEmail), Description = "Email inválido" };
+        }
+
+        public override IdentityError InvalidToken()
+        {
+            return new IdentityError { Code = nameof(InvalidToken), Description = "Token inválido" };
+        }
+    }
+}
