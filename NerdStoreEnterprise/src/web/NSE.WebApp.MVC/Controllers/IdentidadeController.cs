cae3100cd41436b7e552809bc9dc121a6c67c0e2
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-15 20:06:25 -0300

    Criação da estrutura inicial dos métodos da controller de identidade

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioRegistro usuarioRegistro)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+
+    }
+}
