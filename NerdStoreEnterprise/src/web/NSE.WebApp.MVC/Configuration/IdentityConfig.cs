58f91caef481c96abe2a42b74bf4dcb43d10953b
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-15 19:37:15 -0300

    Adicionando as configurações do identity dentro do projeto MVC

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static void AddIdentityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Login";
+                    options.AccessDeniedPath = "acesso-negado";
+                });
+        }
+    }
+}
