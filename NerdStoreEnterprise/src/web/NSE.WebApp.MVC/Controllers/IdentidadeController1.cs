0231ce0ba248774597d2b03d016735a32fed0be0
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-15 19:50:48 -0300

    Criando a controller de identidade no projeto de MVC

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
