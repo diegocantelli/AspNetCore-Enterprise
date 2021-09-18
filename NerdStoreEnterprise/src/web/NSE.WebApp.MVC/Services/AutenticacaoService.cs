14e1483066090851a356701932f4cf209464594f
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-18 16:21:07 -0300

    Criando a interface e a classe de serviço para autenticação

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,21 @@
+﻿using NSE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class AutenticacaoService : IAutenticacaoService
+    {
+        public Task<string> Login(UsuarioLogin usuarioLogin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
