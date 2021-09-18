79e9ae2fbff2852be6f06dcb151f27636682234a
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-18 16:29:54 -0300

    Efetuando o login do usuário através do método post

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -2,15 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
 {
     public class AutenticacaoService : IAutenticacaoService
     {
-        public Task<string> Login(UsuarioLogin usuarioLogin)
+        private readonly HttpClient _httpClient;
+
+        public AutenticacaoService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+        public async Task<string> Login(UsuarioLogin usuarioLogin)
+        {
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(usuarioLogin),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("http://localhost:43967/api/identidade/autenticar", loginContent);
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
         }
 
         public Task<string> Registro(UsuarioRegistro usuarioRegistro)
