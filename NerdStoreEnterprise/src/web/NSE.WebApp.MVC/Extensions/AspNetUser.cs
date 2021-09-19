57612b264ae45b80367934c22989b6b333898a2a
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-19 10:09:05 -0300

    Implementando os métodos responsáveis por recuperar os dados do usuário

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/AspNetUser.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class AspNetUser : IUser
+    {
+        //IHttpContextAccessor -> interface necessária para obter os dados do contexto da requisição
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AspNetUser(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string Name => _httpContextAccessor.HttpContext.User.Identity.Name;
+
+        public bool EstaAutenticado()
+        {
+            return _httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpContext ObterHttpContext()
+        {
+            return _httpContextAccessor.HttpContext;
+        }
+
+        public string ObterUserEmail()
+        {
+            return EstaAutenticado() ? _httpContextAccessor.HttpContext.User.GetUserEmail() : "";
+        }
+
+        public Guid ObterUserId()
+        {
+            return EstaAutenticado() ? Guid.Parse(_httpContextAccessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+
+        public string ObterUserToken()
+        {
+            return EstaAutenticado() ? _httpContextAccessor.HttpContext.User.GetUserToken() : "";
+        }
+
+        public bool Possuirole(string role)
+        {
+            return _httpContextAccessor.HttpContext.User.IsInRole(role);
+        }
+    }
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if(principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst("sub");
+            return claim?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst("email");
+            return claim?.Value;
+        }
+
+        public static string GetUserToken(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst("JWT");
+            return claim?.Value;
+        }
+
+    }
+}
