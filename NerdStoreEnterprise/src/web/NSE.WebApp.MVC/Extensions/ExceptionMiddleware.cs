8a238558691f59edb3f69ec54826714ab2981e67
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-20 18:42:19 -0300

    Criando um middleware para tratar os erros de servidor - unauthorized

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        private async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (CustomHttpRequestException ex)
+            {
+                HandleRequestExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static void HandleRequestExceptionAsync(HttpContext context, CustomHttpRequestException customHttpRequestException)
+        {
+            if(customHttpRequestException.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                context.Response.Redirect("/login");
+                return;
+            }
+
+            context.Response.StatusCode = (int)customHttpRequestException.StatusCode;
+        }
+    }
+}
