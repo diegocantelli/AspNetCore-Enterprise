1a37081fd8b966449ec533e7b2e599dfe1188aa5
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-25 11:27:16 -0300

    Criando a classe de Entidade no projeto NSE.Core, que será a classe base de todas as classes que serão persistidas no BD

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Models/Produto.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Models/Produto.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NSE.Core.DomainObjects;
+using System;
 
 namespace NSE.Catalogo.API.Models
 {
-    public class Produto
+    public class Produto : Entity
     {
         public string Nome { get; set; }
         public string Descricao { get; set; }
