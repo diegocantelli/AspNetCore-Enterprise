6a9fa904e9f571dcd751d4737d045b981324fd5d
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-26 11:38:32 -0300

    Implementando a classe concreta do repositório de produtos

---
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _catalogoContext;
+
+        public ProdutoRepository(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
+
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await _catalogoContext.Produtos.FindAsync(id);
+        }
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await _catalogoContext.Produtos.AsNoTracking().ToListAsync();
+        }
+
+
+        public void Adicionar(Produto produto)
+        {
+            _catalogoContext.Produtos.Add(produto);
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _catalogoContext.Produtos.Update(produto);
+        }
+
+        public void Dispose()
+        {
+            _catalogoContext?.Dispose();
+        }
+
+
+    }
+}
