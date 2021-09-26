051f7b2cdb52db49f512481eb60240a3a4832167
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-26 11:07:03 -0300

    Criando a classe de CatalogoContext da API de Catalogo

---
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options)
+            :base(options){}
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
