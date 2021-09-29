1c03241142b3fb190d50064caff5a54b5f982f9a
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-29 18:43:28 -0300

    Criando a interface do serviço de catálogo e sua classe concreta

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/CatalogoService.cs
@@ -0,0 +1,21 @@
+﻿using NSE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class CatalogoService : ICatalogoService
+    {
+        public Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
