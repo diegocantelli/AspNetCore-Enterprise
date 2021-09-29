6138c613987c882e90eed3cde3d4e50f0152ea19
Diego Cantelli <diegocantelli19@gmail.com>
2021-09-29 18:50:01 -0300

    Implementando os métodos do serviço de catálogo

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/CatalogoService.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/CatalogoService.cs
@@ -1,21 +1,41 @@
-﻿using NSE.WebApp.MVC.Models;
+﻿using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
 {
-    public class CatalogoService : ICatalogoService
+    public class CatalogoService : Service, ICatalogoService
     {
-        public Task<ProdutoViewModel> ObterPorId(Guid id)
+        private readonly HttpClient _httpClient;
+        private readonly AppSettings _appSettings;
+
+        public CatalogoService(HttpClient httpClient, IOptions<AppSettings> settings)
         {
-            throw new NotImplementedException();
+            httpClient.BaseAddress = new Uri(_appSettings.AutenticacaoUrl);
+            _httpClient = httpClient;
+            _appSettings = settings.Value;
+        }
+        public async Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ProdutoViewModel>(response);
         }
 
-        public Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
         {
-            throw new NotImplementedException();
+            var response = await _httpClient.GetAsync("/catalogo/produtos/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
         }
     }
 }
