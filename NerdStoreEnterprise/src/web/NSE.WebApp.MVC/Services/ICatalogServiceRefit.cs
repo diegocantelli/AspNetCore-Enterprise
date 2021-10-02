c303cc0fc2efc7d0e4288ae25919fab09e6e8193
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-02 10:11:31 -0300

    Criando a interface que utiliza o Refit

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/ICatalogServiceRefit.cs
@@ -0,0 +1,18 @@
+﻿using NSE.WebApp.MVC.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public interface ICatalogServiceRefit
+    {
+        [Get("/catalogo/produtos/")]
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+
+        [Get("/catalogo/produtos/{id}")]
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+    }
+}
