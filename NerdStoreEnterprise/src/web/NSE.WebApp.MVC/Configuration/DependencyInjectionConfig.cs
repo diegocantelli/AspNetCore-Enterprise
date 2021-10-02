0d6c70e6ad0aa7be1ee8b8355e5f8361a8443b98
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-02 16:22:05 -0300

    Registrando a dependência que fará uso do Polly

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,15 +19,18 @@ namespace NSE.WebApp.MVC.Configuration
             services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-            //services.AddHttpClient<ICatalogoService, CatalogoService>()
-            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                .AddTransientHttpErrorPolicy(
+                    // em caso de erro, irá tentar 3 vezes a requisição http antes de desistir
+                    p => p.WaitAndRetryAsync(retryCount: 3, sleepDurationProvider: _ => TimeSpan.FromMilliseconds(600)));
 
-            services.AddHttpClient("Refit", options => 
-            {
-                options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
-            })
-            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-            .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+            //services.AddHttpClient("Refit", options => 
+            //{
+            //    options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+            //})
+            //.AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //.AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
 
             // resolvendo a dependência do HttpContextAccessor para ficar disponível de forma singleton
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
