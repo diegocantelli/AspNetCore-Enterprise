ad73e5dc0e9ffa921eb5ac17edce260ee9c89b60
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-03 18:11:32 -0300

    Instalando o Fluent Validation

--- NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/Command.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/Command.cs
@@ -7,5 +7,15 @@ namespace NSE.Core.Messages
     public abstract class Command
     {
         public DateTime TimeStamp { get; private set; }
+
+        public Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
