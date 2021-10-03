e7f69bd85cd6c93ad052e1e8690d781fbed51cac
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-03 18:17:01 -0300

    Criando a classe ClienteCommandHandler

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using NSE.Core.Messages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace NSE.Cliente.API.Application.Commands
 {
-    public class RegistrarClienteCommand
+    public class RegistrarClienteCommand : Command
     {
         public Guid Id { get; private set; }
         public string Nome { get; private set; }
         public string Email { get; private set; }
         public string Cpf { get; private set; }
+
+        public RegistrarClienteCommand(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
     }
 }
