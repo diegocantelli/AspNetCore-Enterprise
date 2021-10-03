d48928e77ac35e97463b28896dac4aced744e989
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-03 11:34:27 -0300

    Criando as classes de Entidade de Cliente e Endereço

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Endereco.cs
@@ -0,0 +1,34 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Endereco : Entity
+    {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Cliente Cliente { get; protected set; }
+
+
+        public Endereco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+
+        }
+    }
+}
+
