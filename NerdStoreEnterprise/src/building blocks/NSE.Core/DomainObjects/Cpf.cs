fc41856eb1a1437814b776aa546fff0d52810a52
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-03 13:36:53 -0300

    Criando uma classe de domain object para cpf e criando uma classe para lançar exceções de domínio

---
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Cpf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Cpf
+    {
+        public const int CpfMaxLength = 11;
+
+        public string Numero { get; private set; }
+
+        // Construtor do EF
+        protected Cpf() { }
+
+        public Cpf(string numero)
+        {
+            if (!Validar(Numero)) throw new DomainException("Cpf inválido.");
+            Numero = numero;
+        }
+
+        public static bool Validar(string numeroCpf) => true;
+    }
+}
