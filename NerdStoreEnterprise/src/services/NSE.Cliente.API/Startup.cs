e7503553120378711608856f1abed8fe7f3a4a7d
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-05 19:19:54 -0300

    Instalando o pacote de injeção de dependência do Mediatr

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Startup.cs
@@ -26,6 +26,8 @@ namespace NSE.Cliente.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddMediatr();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
