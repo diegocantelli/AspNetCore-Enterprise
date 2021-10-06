293ff737e45a5803b9b9641907add6a285d2ad5b
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-06 18:41:16 -0300

    Criando a classe que irá representar o Registro de um Cliente

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation.Results;
 using MediatR;
+using NSE.Core.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,20 @@ using System.Threading.Tasks;
 namespace NSE.Cliente.API.Application.Commands
 {
     // IRequestHandler<RegistrarClienteCommand, ValidationResult> -> é o manipulador daquele que implementa a interface IRequest
-    public class ClienteCommandHandler : IRequestHandler<RegistrarClienteCommand, ValidationResult>
+    public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
         public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
         {
             if (!message.EhValido()) return message.ValidationResult;
 
             //var cliente = new Cliente(message.Id, message.Nome, message.Cpf, message.Email);
+
+            // já existe o cpf no banco
+            if (true)
+            {
+                AdicionarErro("Cpf já cadastrado.");
+                return ValidationResult;
+            }
 
             return message.ValidationResult;
         }
