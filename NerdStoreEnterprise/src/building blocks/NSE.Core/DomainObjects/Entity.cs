12a10dc854141e4e68ffb3003c8e8a4bede2d9d1
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-06 18:53:58 -0300

    Adicionando uma lista de eventos na classe Entity

--- NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Entity.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NSE.Core.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +8,24 @@ namespace NSE.Core.DomainObjects
     public abstract class Entity
     {
         public Guid Id { get; set; }
+        private List<Event> _notificacoes;
+        public IReadOnlyCollection<Event> Notificacoes => _notificacoes.AsReadOnly();
+
+        public void AdicionarEvento(Event evento)
+        {
+            _notificacoes = _notificacoes ?? new List<Event>();
+            _notificacoes.Add(evento);
+        }
+
+        public void RemoverEvento(Event evento)
+        {
+            _notificacoes?.Remove(evento);
+        }
+
+        public void LimparEventos()
+        {
+            _notificacoes?.Clear();
+        }
 
         public override bool Equals(object obj)
         {
