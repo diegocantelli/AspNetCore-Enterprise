c78efbb5fd54527727e0c9d5a5dd146dc2ab06c8
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-10 17:35:37 -0300

    Implementando os métodos da interface na classe concreta MessageBus

---
+++ NerdStoreEnterprise/src/building blocks/NSE.MessageBus/IMessageBus.cs
@@ -0,0 +1,35 @@
+﻿using NSE.Core.Messages.Integration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.MessageBus
+{
+    public interface IMessageBus : IDisposable
+    {
+        void Publish<T>(T message) where T : IntegrationEvent;
+        Task PublishAsync<T>(T message) where T : IntegrationEvent;
+        void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
+        void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+
+        TResponse Request<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        IDisposable Respond<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        bool IsConnected { get; }
+
+    }
+}
--- NerdStoreEnterprise/src/building blocks/NSE.MessageBus/MessageBus.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.MessageBus/MessageBus.cs
@@ -7,33 +7,84 @@ namespace NSE.MessageBus
 {
     public class MessageBus : IMessageBus
     {
+        private readonly string _connectionString;
         private IBus _bus;
-    }
 
-    public interface IMessageBus : IDisposable
-    {
-        void Publish<T>(T message) where T : IntegrationEvent;
-        Task PublishAsync<T>(T message) where T : IntegrationEvent;
-        void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
-        void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+        public MessageBus(string connectionString)
+        {
+            _connectionString = connectionString;
 
-        TResponse Request<TRequest, TResponse>(TRequest request)
+            TryConnect();
+        }
+
+        public bool IsConnected => _bus?.IsConnected ?? false;
+
+        public void Dispose()
+        {
+            _bus?.Dispose();
+        }
+
+        public void Publish<T>(T message) where T : IntegrationEvent
+        {
+            TryConnect();
+            _bus.Publish(message);
+        }
+
+        public Task PublishAsync<T>(T message) where T : IntegrationEvent
+        {
+            TryConnect();
+            return _bus.PublishAsync(message);
+        }
+
+        public TResponse Request<TRequest, TResponse>(TRequest request)
             where TRequest : IntegrationEvent
-            where TResponse : ResponseMessage;
+            where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.Request<TRequest, TResponse>(request);
+        }
 
-        Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+        public Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
             where TRequest : IntegrationEvent
-            where TResponse : ResponseMessage;
+            where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.RequestAsync<TRequest, TResponse>(request);
+        }
 
-        IDisposable Respond<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+        public IDisposable Respond<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
             where TRequest : IntegrationEvent
-            where TResponse : ResponseMessage;
+            where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.Respond(responder);
+        }
 
-        IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+        public IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
             where TRequest : IntegrationEvent
-            where TResponse : ResponseMessage;
+            where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.RespondAsync(responder);
+        }
 
-        bool IsConnected { get;  }
+        public void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class
+        {
+            TryConnect();
+            _bus.Subscribe(subscriptionId, onMessage);
+        }
 
+        public void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
+        {
+            TryConnect();
+            _bus.SubscribeAsync(subscriptionId, onMessage);
+        }
+
+        private void TryConnect()
+        {
+            if (IsConnected) return;
+
+            _bus = RabbitHutch.CreateBus(_connectionString);
+        }
     }
 }
