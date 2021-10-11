e57797974801789b4fe39668036b70c72c6b805c
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-11 19:03:22 -0300

    Criando algumas extensões para facilitar a configuração das filas e a leitura das informações nos appSettings

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Configuration/MessageBusConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Core.Utils;
+using NSE.MessageBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
+        }
+    }
+}
